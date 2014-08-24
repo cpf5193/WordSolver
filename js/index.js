540ec885ff59fc95352bc1e7ff273db331f5382a
cpf5193 <cpf5193@cs.washington.edu>
2014-08-24 12:58:10 -0700

    Style grid, add input validation

--- js/index.js
+++ js/index.js
@@ -2,7 +2,38 @@ $(function(){
   // Get this later from a form
   var NUM_TILES = 16;
   drawGrid(NUM_TILES);
+  sanitizeKeystrokes();
 });
+
+function sanitizeKeystrokes() {
+  var allowedKeys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                     's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+                     'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                     'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];  
+  $('.tile input').keyup(function(e) {
+    var charCode = e.keyCode;
+    var content = this.value, currentChar;
+    for(var i=0; i<content.length; ++i) {
+      currentChar = content.charAt(i);
+      if (allowedKeys.indexOf(currentChar) === -1) {
+        content = content.slice(0, i) + content.slice(i+1, content.length);
+        --i;
+      }
+    }
+    this.value = content;
+    // if (charCode < 65 || charCode > 90) {
+    //   content = this.value;
+    //   firstChar = content.charAt(0);
+    //   if (content.length > 1 && (allowedKeys.indexOf(firstChar) != -1)) {
+    //     this.value = content.charAt(0);
+    //   } else {
+    //     this.value = '';
+    //   }
+    // }
+  });
+}
 
 function drawGrid(numTiles) {
   // Render the rows
